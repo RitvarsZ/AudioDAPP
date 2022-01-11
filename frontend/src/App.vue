ca464448097b30b7a636bfae0abc758170acdcf2
Ritvars Zvejnieks <ritvarszvejnieks@protonmail.com>
2022-01-11 18:37:40 +0200

    eh

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,27 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <header class="flex justify-evenly py-7 bg-gray-900
+  text-yellow-50 font-bold justify-center">
+    <nav>
+      <ul class="flex gap-4">
+        <li class="hover:text-purple-400 transition">
+          <router-link to="/">Home</router-link>
+        </li>
+        <li class="hover:text-purple-400 transition">
+          <router-link to="/marketplace">Marketplace</router-link>
+        </li>
+      </ul>
+    </nav>
+    <div class="float-right">Connect</div>
+  </header>
+  <main class="text-gray-100 bg-gray-800 flex flex-col h-screen">
+    <div class="my-16 mx-6 md:mx-16 xxl:mx-64">
+      <router-view/>
+    </div>
+  </main>
+  <footer class="bg-gray-800 text-gray-100">
+    <hr class="opacity-25 mx-36">
+    <sm class="pt-16 pb-8 flex justify-center font-light text-sm opacity-80">rz18025 @2022</sm>
+  </footer>
 </template>
 
 <style>
@@ -11,20 +29,5 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="flex">
+      <h1 class="text-7xl font-bold bg-gray-100 text-purple-600 pb-7">
+        Register a record
+      </h1>
+    </div>
+    <div>file upload</div>
+
+    <div class="flex mt-12">
+      <h1 class="text-7xl font-bold bg-gray-100 text-purple-600 pb-7">
+        Check for a record
+      </h1>
+    </div>
+    <div>file upload</div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  components: {},
 });
 </script>
