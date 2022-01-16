9545ca8a0df3d8db96e220c9d7e6a60ebb8557d9
Ritvars Zvejnieks <ritvarszvejnieks@protonmail.com>
2022-01-16 16:58:24 +0200

    Remove fluentUI.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,3 @@
-import { provideFluentDesignSystem, fluentCard, fluentButton } from '@fluentui/web-components';
 import './index.css';
 import { createApp } from 'vue';
 import App from './App.vue';
@@ -6,7 +5,3 @@ import router from './router';
 import store from './store';
 
 createApp(App).use(store).use(router).mount('#app');
-provideFluentDesignSystem().register(
-  fluentCard(),
-  fluentButton(),
-);
